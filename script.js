461337f59992587a21f5ee7d12ae4163bb5ebcde
Vladimir <volodya.voronkin@gmail.com>
2023-01-28 23:46:31 +0200

    user_bio

---
+++ script.js
@@ -0,0 +1,9 @@
+const date = prompt("Год рождения")
+const new_date = Number(date)
+const name = prompt("Имя")
+const surname = prompt("Фамилия")
+const age = 2023 - new_date
+console.log(name, surname, age)
+alert(age)
+alert(name)
+alert(surname)
