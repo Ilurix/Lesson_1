a6200a845bf54f831616cf274d0d73d8a04775da
Vladimir <volodya.voronkin@gmail.com>
2023-02-14 00:35:30 +0200

    Astrology

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const userBirth = +prompt("Год рождения");
-const userName = prompt("Имя");
-const userSurname = prompt("Фамилия");
-const currentYear = 2023;
-const userAge = currentYear - userBirth;
-console.log(userName, userSurname, userAge);
-alert(`User Bio: ${userName} ${userSurname}, ${userAge} Year old;`);
+let userName = prompt('Имя');
+let userSurname = prompt('Фамилия');
+let userBirthDay = +prompt('День рождения');
+let userBirthMonths = +prompt('Месяц рождения');
+let userBirthYear = +prompt('Год рождения');
+let currentYear = 2022;
+let userAge = currentYear - userBirth;
+document.whrite(`User Bio: ${userName}, ${userSurname},${userAge}`)
+if (userBirthYear % 4 == 0 || userBirthYear % 400 == 0) {
+    document.write('leap year')
+}
+if (userBirthMonths == 1 && userBirthDay >= 20 || userBirthMonths == 2 && userBirthDay >= 18) {
+    document.write('Aquarius ♒')
+}
